337e6b0e5626c23544b8fde4a19b7cb09ca6416f
Jorge Ikeda <freebot@gmail.com>
2025-03-23 22:39:28 -0600

    Tercer commit

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import os
 from dotenv import load_dotenv
 from pydantic import BaseModel
 from langchain_openai import  ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser 
+from langchain.agents import create_tool_calling_agent, AgentExecutor
 
 load_dotenv()
 # Obtener la clave de API desde el entorno
@@ -9,7 +12,36 @@ api_key = os.getenv("OPENAI_API_KEY")
 if not api_key:
     raise ValueError("No se encontró la clave de API en las variables de entorno")
 
-
+class ResearchResponse(BaseMOdel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+    
 llm = ChatOpenAI(model="gpt-4o-mini", openai_api_key=api_key)
-response=llm.invoke("what is the meaning of life?")
-print(response)
+parser=PydanticOutputParser(pydantic_object=ResearchResponse)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a research assistant that will help generate a research paper.
+            Answer the user query and use neccessary tools. 
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+).partial(format_instructions=parser.get_format_instructions())
+
+agent = create_tool_calling_agent(
+    llm=llm,
+    prompt=prompt,
+    tools=[]
+)
+
+agent_execuotr = AgentExecutor(agent=agent, tools=[], verbose=True, )
+raw_response = agent_executor.invoke({"query": "What is the capital of France?"})
